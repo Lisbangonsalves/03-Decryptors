af25db82d3edd04cb1ad9065f1d087e8c1e8933d
Axill <axilldcunha@gmail.com>
2022-10-07 12:20:59 +0530

    ejs done

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  avatar: {
+    type: String
+  }
+
+});
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const path = require("path");
+const expressLayouts = require("express-ejs-layouts");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+
+
+const port = process.env.PORT || 5000;
+const ConnectionDB = require("./database");
+ConnectionDB();
+
+app.use(express.json());
+
+
+//seting static files
+app.use(express.static("public")); //for servinhg staticfile
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/js", express.static(__dirname + "public/js"));
+app.use("/img", express.static(__dirname + "public/img"));
+
+//setting view engine to EJS
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+
+app.get("/", (req, res) => {
+    res.render("index");
+  });
+  
+  
+// app
+//   app.use("/api/image",require("./multer"))
+//   app.use("/api/auth",require("./routes/Auth/userAuth"))
+app.listen(port, () => console.log(`Server up and running...at ${port}`))
